3d9dab872e66dc24173e7b433e36a272ad9e4ffd
Josué Melquisedeque Leite <84863364+josuemleite@users.noreply.github.com>
2022-12-05 17:11:32 -0300

    footer and variables insertions

--- JS/scroll.js
+++ JS/scroll.js
@@ -1,12 +1,12 @@
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop()
 
-    if (scroll > 50) {
-        $('.navbar').css('transition-duration','0.7s');
-        $('.navbar').css('background-color','whitesmoke');
-        $('.img-responsive').css('filter', 'grayscale(0%)');
-    } else {
-        $('.navbar').css('background-color','transparent');
-        $('.img-responsive').css('filter', 'grayscale(60%)');
-    }
-});
+  if (scroll > 50) {
+    $('.navbar').css('transition-duration', '0.7s')
+    $('.navbar').css('background-color', 'whitesmoke')
+    $('.img-responsive').css('filter', 'grayscale(0%)')
+  } else {
+    $('.navbar').css('background-color', 'transparent')
+    $('.img-responsive').css('filter', 'grayscale(60%)')
+  }
+})
